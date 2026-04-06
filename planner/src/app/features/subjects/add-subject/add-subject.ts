50549eee89e107123c37ee81beeb1f49ca60f0ac
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-04-06 22:15:16 +0530

    Added add subject header and subject cards

--- planner/src/app/features/subjects/add-subject/add-subject.ts
+++ planner/src/app/features/subjects/add-subject/add-subject.ts
@@ -3,7 +3,11 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
 import { ButtonModule } from 'primeng/button';
-
+import { NgForOf } from "../../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { CommonModule } from '@angular/common';
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 interface Subject {
   name: string;
   difficulty: string;
@@ -13,13 +17,76 @@ interface Subject {
 @Component({
   selector: 'app-add-subject',
   standalone: true,
-  imports: [ReactiveFormsModule, InputTextModule, SelectModule, ButtonModule],
+  imports: [ReactiveFormsModule, InputTextModule, SelectModule, ButtonModule, CommonModule,DialogModule,FormsModule,AutoCompleteModule],
   templateUrl: './add-subject.html',
   styleUrl: './add-subject.scss'
 })
 export class AddSubject implements OnInit {
+  subjects = [
+  {
+    name: 'Data Structures',
+    description: 'Practice arrays, linked list, trees',
+  },
+  {
+    name: 'DBMS',
+    description: 'Learn SQL and normalization',
+  },
+  {
+    name: 'Operating Systems',
+    description: 'Process, threads, scheduling',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+  {
+    name: 'Computer Networks',
+    description: 'OSI model and protocols',
+  },
+];
 
  ngOnInit(): void {
    
  }
+ 
 }
