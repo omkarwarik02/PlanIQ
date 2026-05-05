a00448cb8f00811c48aa15b1630a47f2e560b8e0
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-05-05 23:13:15 +0530

    task-component

--- planner/src/app/features/tasks-component/tasks-component.ts
+++ planner/src/app/features/tasks-component/tasks-component.ts
@@ -1,4 +1,4 @@
-import { Component,inject } from '@angular/core';
+import { Component,computed,inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
@@ -12,6 +12,10 @@ FormsModule
 })
 export class TasksComponent {
   private aiTask = inject(AiTask);
+
+  tasks = this.aiTask.tasks;
+
+  isLoading = signal(false);
   isChecked: boolean = false;
 
 
@@ -22,10 +26,19 @@ export class TasksComponent {
     
   }
 
+
+  alltasks = computed(() => this.tasks().flatMap(g=>g.tasks));
+
   generateTasks(){
+    this.isLoading.set(true);
     this.aiTask.generateTasks().subscribe({
       next:() =>{
         console.log("Task Created");
+        this.isLoading.set(false);
+      },
+      error:(err)=>{
+        console.error(err.message);
+        this.isLoading.set(false);
       }
     })
   }
