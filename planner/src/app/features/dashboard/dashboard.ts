a454a2f5b54a4e50860edcb2ed998d166fffd9ef
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-05-06 22:32:26 +0530

    Worked on dashboard component cards

--- planner/src/app/features/dashboard/dashboard.ts
+++ planner/src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed, OnInit } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { CommonModule } from '@angular/common';
+import { AddsubService } from '../../services/addsub.service';
+import { AiTask } from '../../services/ai-task';
+import { AiService } from '../../services/ai.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ChartModule, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  private subjectService = inject(AddsubService);
+  private aiTask = inject(AiTask);
+  private aiservice = inject(AiService);
 
+  subjects = this.subjectService.subjects;
+  tasks = this.aiTask.tasks;
+  duration = this.aiservice.plan;
+
+  totalSubjects = computed(() => this.subjects().length);
+  totalHours = computed(() => this.subjects().reduce((total, s) => total + (s.hours ?? 0), 0));
+
+  ngOnInit() {
+    this.subjectService.getSubjects().subscribe();
+  }
 }
