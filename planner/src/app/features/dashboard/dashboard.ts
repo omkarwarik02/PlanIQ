4986c2129167319d11d6ed247925f0ac3604d463
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-05-07 00:22:51 +0530

    changed and fixed dashboard component

--- planner/src/app/features/dashboard/dashboard.ts
+++ planner/src/app/features/dashboard/dashboard.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AddsubService } from '../../services/addsub.service';
 import { AiTask } from '../../services/ai-task';
 import { AiService } from '../../services/ai.service';
+import { AiUsage } from '../../services/ai-usage';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,16 +16,19 @@ export class Dashboard implements OnInit {
   private subjectService = inject(AddsubService);
   private aiTask = inject(AiTask);
   private aiservice = inject(AiService);
+  private aiUsage = inject(AiUsage);
 
   subjects = this.subjectService.subjects;
   tasks = this.aiTask.tasks;
   duration = this.aiservice.plan;
+  aiUsageCount = this.aiUsage.usage;
 
   totalSubjects = computed(() => this.subjects().length);
   totalHours = computed(() => this.subjects().reduce((total, s) => total + (s.hours ?? 0), 0));
   totaltasks = computed(() => this.tasks().flatMap(g=>g.tasks).length);
   ngOnInit() {
     this.subjectService.getSubjects().subscribe();
+    this.aiUsage.usage();
     
   }
 }
